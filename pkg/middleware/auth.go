d81b23e521060883fa12854f0e802f409277b4fa
Alexander <aalexanderbn@icloud.com>
2025-11-20 23:54:38 +0300

    middleware для авторизации. расшифровка токена

---
+++ pkg/middleware/auth.go
@@ -0,0 +1,46 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"go/api/configs"
+	"go/api/pkg/jwt"
+	"net/http"
+	"strings"
+)
+
+type key string
+
+const (
+	ContextPhoneKey key = "ContextPhoneKey"
+)
+
+func writeUnauthed(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write([]byte(http.StatusText(http.StatusUnauthorized)))
+}
+
+func IsAuthed(next http.Handler, config *configs.Config) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authedHeader := r.Header.Get("Authorization")
+		if !strings.HasPrefix(authedHeader, "Bearer") {
+			writeUnauthed(w)
+			return
+		}
+		token := strings.TrimPrefix(authedHeader, "Bearer")
+		isValid, data := jwt.NewJWT(config.MyUser.Secret).Pasre(token)
+		if !isValid {
+			writeUnauthed(w)
+			return
+		}
+		fmt.Println(token)
+		fmt.Println(isValid)
+		fmt.Println(data)
+
+		ctx := context.WithValue(r.Context(), ContextPhoneKey, data.Phone)
+		req := r.WithContext(ctx)
+
+		next.ServeHTTP(w, req)
+	})
+
+}
